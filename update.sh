7dd86e20e417997c3bd938908e32f3f152f81b3e
Scott Campbell <scottc@users.noreply.github.com>
2025-10-01 10:10:05 +0800

    Update the upgrade script

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-cd /etc/nixos && sudo nix flake update && sudo nixos-rebuild switch
+# Update the lock file, with new pinned versions
+nix flake update --flake .
 
+# TODO: commit changes to git, then deploy
+
+# Deploy to to /etc/nixos
+sudo cp *.{nix,lock} /etc/nixos/
+
+# Apply Build
+sudo nixos-rebuild switch --flake /etc/nixos#nixos
