879cb0be6238879f0c915caa5b1d6df70c0005f2
scottc <scottc@users.noreply.github.com>
2025-11-01 20:05:15 +0800

    nix flake update

--- home.nix
+++ home.nix
@@ -12,25 +12,57 @@
   };
 
   home.packages = [
-    # pkgs.hello
-    # pkgs.kitty
-    # pkgs.kdePackages.dolphin
+    # App Launchers
     # pkgs.wofi
+
+    # File Managers
+    # pkgs.kdePackages.dolphin
+
+    # Web Browsers
     pkgs.brave
     pkgs.ladybird
-    pkgs.hexchat
-    pkgs.mpv
-    pkgs.libreoffice
 
-    # pkgs.git
+    # VCS ((Distributed) Version Control Systems)
     pkgs.jujutsu # A Distributed VCS, frontend like git, but with multiple backends, like git.
     pkgs.gg # a gui for jujutsu
+    # pkgs.git
 
+    # AI - Artifical Inteligence
     pkgs.lmstudio
 
-    pkgs.lutris
-    # pkgs.space-cadet-pinball
+    # Social & Communications
+    pkgs.hexchat
 
+    # Multimedia
+    pkgs.mpv
+    pkgs.lbry
+
+    # Office Productivity Apps
+    pkgs.libreoffice
+
+    # Creative Productivity Apps
+    pkgs.gimp
+    pkgs.inkscape
+
+    # Game Managers
+    pkgs.lutris
+
+    # Games
+    # pkgs.space-cadet-pinball
+    # pkgs.superTuxKart
+
+    # Shells
+    # pkgs.kitty
+
+    # Code editors
+    pkgs.lapce # Code editor
+    pkgs.helix # Code editor
+    pkgs.zed-editor # Code editor
+    pkgs.vscodium # Code editor
+
+    # Compilers - Zig... probably install this in a nix develop environment, not per user.
+    # pkgs.zig # Zig Compiler
+    # pkgs.zls # Zig Language Server
 
     # Install build tools and language servers via project specific flake,
     # so we can have per project versions installed.
@@ -38,19 +70,8 @@
     # then run the desired code editor from within the dev environment,
     # which should detect the build tools and language server.
     #
-    # pkgs.zig # Zig Compiler
-    # pkgs.zls # Zig Language Server
+    # TODO: enable a nix language server to help lint these nix files...
     # pkgs.nil # nil (nix) Language Server
-
-    pkgs.lapce # Code editor
-    pkgs.helix # Code editor
-    pkgs.zed-editor # depends on nodejs, which needs to be compiled from source and last time laptop went unresponsive
-    pkgs.vscodium # Code editor
-
-    pkgs.superTuxKart
-    pkgs.gimp
-    pkgs.inkscape
-    pkgs.lbry
   ];
 
   # programs.steam = {
@@ -80,9 +101,4 @@
 
   # Optional, hint Electron apps to use Wayland:
   # home.sessionVariables.NIXOS_OZONE_WL = "1";
-
-
-
-
-
 }
