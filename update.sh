965dbca052c60430dd50869ed26c9c9a73bbc2c0
scottc <scottc@users.noreply.github.com>
2025-11-09 14:28:03 +0800

    nix flake update

--- update.sh
+++ update.sh
@@ -3,9 +3,13 @@
 # Fix dirty warning
 git add *.{nix,lock,sh}
 
+# Disabled updates, because some python package breaking the build... need to find & fix or remove the dependency...
+# building python3.13-pyrate-limiter-3.9.0 (pytestCheckPhase): ........................................................................ [ 23%]
+
 # Update the lock file, with new pinned versions
 # nix flake update --flake .
 
+# Fix dirty warning
 git add *.{nix,lock,sh}
 
 # Deploy to to /etc/nixos
