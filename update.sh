abfcd042b9cab64bd6c92427c35e7188c62f619a
scottc <scottc@users.noreply.github.com>
2025-11-09 14:23:31 +0800

    nix flake update

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Update the lock file, with new pinned versions
-nix flake update --flake .
+# Fix dirty warning
+git add *.{nix,lock,sh}
 
-# Save changes to git
-git add *.{nix,lock}
-git commit -m "nix flake update"
-git push
+# Update the lock file, with new pinned versions
+# nix flake update --flake .
+
+git add *.{nix,lock,sh}
 
 # Deploy to to /etc/nixos
 sudo cp *.{nix,lock} /etc/nixos/
@@ -14,3 +14,6 @@ sudo cp *.{nix,lock} /etc/nixos/
 # Apply Build
 sudo nixos-rebuild switch --flake /etc/nixos#nixos
 
+# Save changes to git
+git commit -m "nix flake update"
+git push
